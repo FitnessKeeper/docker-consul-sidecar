ae788c6971e9ac7d44c05ec9c45b9db78fdfff69
Tim Hartmann <tfhartmann@gmail.com>
2018-02-15 10:51:59 -0500

    Adding ECS Cluster services
    This should discover what cluster the check in running on, and register
    a service

---
+++ check_definitions/ecs-cluster.sh
@@ -0,0 +1,30 @@
+!#/bin/bash
+
+ECS_CLUSTER=$(curl http://localhost:51678/v1/metadata | jq -r .Cluster)
+DOCKER_ID=$(head -1 /proc/self/cgroup  | cut -d'/' -f4)
+
+_SERVICE=$(cat <<EOT
+{
+  "service": {
+    "name": "${ECS_CLUSTER}",
+    "address": "",
+    "tags": [
+      "ecs"
+    ],
+    "checks": [
+      {
+        "id": "ami-up2date",
+        "name": "AMI Status",
+        "docker_container_id": "${DOCKER_ID}",
+        "shell": "/bin/bash",
+        "args": ["/usr/local/bin/ami_up2date.sh"],
+        "interval": "10s",
+        "status": "passing"
+      }
+    ]
+  }
+}
+EOT
+)
+
+echo ${_SERVICE}
