daca2179ff93a018a938c39c80d30fd65482e4ce
Tim Hartmann <tfhartmann@gmail.com>
2018-03-01 17:00:13 -0500

    Change to count registered instances

--- scripts/ecs-cloudwatch-metrics.sh
+++ scripts/ecs-cloudwatch-metrics.sh
@@ -8,9 +8,9 @@
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//')
 ECS_CLUSTER=$(curl -s http://localhost:51678/v1/metadata | jq -r .Cluster)
 ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+CONTAINERINSTANCES=$(aws --region ${REGION} ecs describe-clusters --clusters ${ECS_CLUSTER} | jq .clusters[0].registeredContainerInstancesCount)
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 #aws --region ${REGION} cloudwatch put-metric-data --metric-name ContainerInstancesCount --namespace /AWS/ECS --dimensions Cluster=${ECS_CLUSTER},InstanceId=${ID} --unit Count --value 1 --timestamp ${TIMESTAMP}
-aws --region ${REGION} cloudwatch put-metric-data --metric-name ContainerInstancesCount --namespace /AWS/ECS/${ECS_CLUSTER} --unit Count --value 1 --timestamp ${TIMESTAMP}
-echo "${TIMESTAMP} cluster=${ECS_CLUSTER} instance-id=${ID}"
+aws --region ${REGION} cloudwatch put-metric-data --metric-name registeredContainerInstances --namespace /AWS/ECS --dimensions Cluster=${ECS_CLUSTER} --unit Count --value ${CONTAINERINSTANCES} --timestamp ${TIMESTAMP}
 exit 0
