2a1771854a8b2c7fd510a67f4fa7a4e8fc0b1193
Tim Hartmann <tfhartmann@gmail.com>
2018-03-01 16:15:28 -0500

    Adding some logging
    Also changing up what the metric is by removing dimensions

--- scripts/ecs-cloudwatch-metrics.sh
+++ scripts/ecs-cloudwatch-metrics.sh
@@ -10,5 +10,7 @@ ECS_CLUSTER=$(curl -s http://localhost:51678/v1/metadata | jq -r .Cluster)
 ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-aws --region ${REGION} cloudwatch put-metric-data --metric-name ContainerInstancesCount --namespace /AWS/ECS --dimensions Cluster=${ECS_CLUSTER},InstanceId=${ID} --unit Count --value 1 --timestamp ${TIMESTAMP}
+#aws --region ${REGION} cloudwatch put-metric-data --metric-name ContainerInstancesCount --namespace /AWS/ECS --dimensions Cluster=${ECS_CLUSTER},InstanceId=${ID} --unit Count --value 1 --timestamp ${TIMESTAMP}
+aws --region ${REGION} cloudwatch put-metric-data --metric-name ContainerInstancesCount --namespace /AWS/ECS/${ECS_CLUSTER} --unit Count --value 1 --timestamp ${TIMESTAMP}
+echo "${TIMESTAMP} cluster=${ECS_CLUSTER} instance-id=${ID}"
 exit 0
