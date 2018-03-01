46e316543682e9016e4897e79cefc6b7f54b9087
Tim Hartmann <tfhartmann@gmail.com>
2018-03-01 15:35:12 -0500

    Added Unit and more Dimensions

--- scripts/ecs-cloudwatch-metrics.sh
+++ scripts/ecs-cloudwatch-metrics.sh
@@ -9,5 +9,5 @@ REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-
 ECS_CLUSTER=$(curl -s http://localhost:51678/v1/metadata | jq -r .Cluster)
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-aws --region ${REGION} cloudwatch put-metric-data --metric-name ContainerInstancesCount --namespace /AWS/ECS --dimensions Cluster=${ECS_CLUSTER} --value 1 --timestamp ${TIMESTAMP}
+aws --region ${REGION} cloudwatch put-metric-data --metric-name ContainerInstancesCount --namespace /AWS/ECS --dimensions Cluster=${ECS_CLUSTER} --unit Count --value 1 --timestamp ${TIMESTAMP}
 exit 0
