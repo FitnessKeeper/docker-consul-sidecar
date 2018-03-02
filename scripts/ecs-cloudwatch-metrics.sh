210784dbc7b6101fa6345a46a14f4e2563662440
Tim Hartmann <tfhartmann@gmail.com>
2018-03-02 12:21:02 -0500

    comment out unused var
    Since we aren't using that var at the moment, I commented it out, this
    should also make the runtime faster

--- scripts/ecs-cloudwatch-metrics.sh
+++ scripts/ecs-cloudwatch-metrics.sh
@@ -7,7 +7,7 @@
 
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//')
 ECS_CLUSTER=$(curl -s http://localhost:51678/v1/metadata | jq -r .Cluster)
-ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+#ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 CONTAINERINSTANCES=$(aws --region ${REGION} ecs describe-clusters --clusters ${ECS_CLUSTER} | jq .clusters[0].registeredContainerInstancesCount)
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
